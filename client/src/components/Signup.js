bf5a3e231a65de203b847e56cae4f1610fa5875a
sotomaque <sotomaque@gmail.com>
2020-01-31 13:56:58 -0800

    first commit

---
+++ client/src/components/Signup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Signup extends React.Component {
+    render() {
+        return (
+            <div>Sign Up</div>
+        );
+    }
+};
+
+export default Signup;
